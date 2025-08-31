50481f0cd453e8ba017208e7e9c4ec0b2cd952dd
Amit Bisht <am.bisht2003@gmail.com>
2025-08-31 11:33:41 +0530

    Add a new file comment.py

---
+++ Day1_Basics/comment.py
@@ -0,0 +1,19 @@
+# Comments can be used to explain Python code. 
+
+#                       CREATING A COMMENT
+#  Comments starts with a #, and Python will ignore them :
+
+#This is a Comment line, you are reading.
+print("Hello, World!")
+
+
+#                       MULTILINE COMMENTS
+# Using Triple Quotes """   """
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("I am coding.")
+
