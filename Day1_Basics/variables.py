9f40ff957bd70a5958bc13e6fb2816636fd75e47
Amit Bisht <am.bisht2003@gmail.com>
2025-09-01 23:19:48 +0530

    Vairables Declaration

---
+++ Day1_Basics/variables.py
@@ -0,0 +1,18 @@
+#VARIABLES are containers for storing data values.
+#------------------------------------------------------------
+
+# 1, Basic Variables declaration
+name = "Amit Bisht"  
+age = 21
+height = 5.8
+print("Name:",name)
+print("Age:", age)
+print("Height:", height)
+#____________________________________________
+
+#2. Different Data Types
+city = "Haldwani"
+roll_number = 1
+cgpa = 8.5
+is_hosteller = "True"
+print("city:,type(city)","roll_number:, type(roll_number)" ,"cgpa: type(cgpa)" , "is_hosteller: type(is_hosteller)")
