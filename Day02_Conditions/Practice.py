398a120a4d0908e3e1728c67f05fb91f60e006db
Amit Bisht <am.bisht2003@gmail.com>
2025-09-07 23:45:17 +0530

    Add Day02_Condition: Practice File

---
+++ Day02_Conditions/Practice.py
@@ -0,0 +1,8 @@
+#Number is Positive, negative or Zero
+num = int(input("Enter the number: "))
+if num==0:
+    print(f"Number {num} is Zero.")
+elif num > 0:
+    print(f"Number {num} is Posivitive.")
+else:
+    print(f"Number {num} is Negative.")        
