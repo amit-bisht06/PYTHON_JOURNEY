d97e6c5db7eb6c48e14b969288a8436b16e1225d
Amit Bisht <am.bisht2003@gmail.com>
2025-09-08 00:17:43 +0530

    Added match_statement.py files

---
+++ Day02_Conditions/match_statement.py
@@ -0,0 +1,17 @@
+# Instead of writing many IF...ELSE statements, you can use the MATCH statement.
+day = 3
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
