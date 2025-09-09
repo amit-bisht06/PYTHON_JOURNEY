0d5e1caa2d3c915581dace611d2931295b581b0e
Amit Bisht <am.bisht2003@gmail.com>
2025-09-09 15:59:05 +0530

    Added Day09: practice.py files

---
+++ Day09 Lambda FUnction/practice.py
@@ -0,0 +1,8 @@
+"""
+Practice Questions:
+1. Write a lambda function to calculate the cube of a number.
+2. Use map() with lambda to convert a list of strings into uppercase.
+3. Use filter() with lambda to find all numbers greater than 10 in a list.
+4. Use reduce() with lambda to find the product of all numbers in a list.
+5. Sort a list of dictionaries using a lambda function (by a specific key).
+"""
