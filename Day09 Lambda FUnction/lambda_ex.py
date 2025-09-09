8c7a9986dbde3458b5f2556f806885a34ad2a6d2
Amit Bisht <am.bisht2003@gmail.com>
2025-09-09 15:11:04 +0530

    Added Day09_lambda: lambda_examples

--- Day09 Lambda FUnction/lambda_ex.py
+++ Day09 Lambda FUnction/lambda_ex.py
@@ -12,3 +12,7 @@ print("Square using lambda: ", square_lambda(3))
 # Lambda with two arguments
 add = lambda x,y : x+y 
 print("Sum using lambda: ", add(3,4))
+
+# Lambda with Condition
+max_num = lambda x,y: x if x>y else y
+print("Maximum number: ", max_num(6,8))
