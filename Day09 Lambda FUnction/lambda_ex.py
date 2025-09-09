6174f874a0952af8114faa261e330316904ba48d
Amit Bisht <am.bisht2003@gmail.com>
2025-09-09 15:39:10 +0530

    Added Day09_lambda: lambda_examples

--- Day09 Lambda FUnction/lambda_ex.py
+++ Day09 Lambda FUnction/lambda_ex.py
@@ -1,18 +1,43 @@
 # Lambda Functions in Python
+#A lambda function is a small anonymous function.
+
+# A lambda function can take any number of arguments, but can only have one expression.
+#-------------------------------------------------------------------------------------------------------
 
 # Normal function
 def square(x):
     return x*x
 print("Square using function is: ",square(5))
+print()
+#------------------------------------------------------------------------------
 
 # Same using lambda
 square_lambda = lambda x: x*x
 print("Square using lambda: ", square_lambda(3))
+print()
+#------------------------------------------------------------------------------
 
 # Lambda with two arguments
 add = lambda x,y : x+y 
 print("Sum using lambda: ", add(3,4))
+print()
+#------------------------------------------------------------------------------
 
 # Lambda with Condition
 max_num = lambda x,y: x if x>y else y
 print("Maximum number: ", max_num(6,8))
+print()
+#------------------------------------------------------------------------------
+
+# Using map with lambda
+nums = [1, 2, 3, 4, 5]
+square_list = list(map(lambda x : x**2, nums))
+print("Square of list: ", square_list)
+print()
+#------------------------------------------------------------------------------
+
+# Using filter with lambda
+even_num = list(filter(lambda x : x%2==0, nums))
+print("Even Number List: ", even_num)
+print()
+#-------------------------------------------------------------------------------
