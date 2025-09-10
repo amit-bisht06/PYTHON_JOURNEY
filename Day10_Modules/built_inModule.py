93378ed463f19e2c9486bba9b1c81bfe3d344807
Amit Bisht <am.bisht2003@gmail.com>
2025-09-10 14:22:18 +0530

    Added Day10_Module: built_inModule.py  file

---
+++ Day10_Modules/built_inModule.py
@@ -0,0 +1,8 @@
+"""
+* Using Python's built-in math module
+"""
+import math
+print("Square root of 16:", math.sqrt(16))
+print("Factorial of 5:", math.factorial(5))
+print("Pi value:", math.pi)
+print("Sine of 90 degrees:", math.sin(math.radians(90)))
