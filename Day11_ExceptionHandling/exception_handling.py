52085a9d30281adcb2414c0cc9c7c998c01f0334
Amit Bisht <am.bisht2003@gmail.com>
2025-09-11 16:38:33 +0530

    Added Day11_ExceptionHandline: exceptionHandline.py file

--- Day11_ExceptionHandling/exception_handling.py
+++ Day11_ExceptionHandling/exception_handling.py
@@ -3,6 +3,4 @@ try:
     num= int(input("Enter a number: "))
     print("You Entered: ", num)
 except ValueError:
-    print("Invalid Input! Please enter a number")
-
-    
+    print("Invalid Input! Please enter a number") 
