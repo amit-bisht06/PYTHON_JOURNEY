dc7410c527d941377d405259e5bb09233a7f99bc
Amit Bisht <am.bisht2003@gmail.com>
2025-09-11 16:44:25 +0530

    Added Day11_ExceptionHandline: exceptionHandline.py file

--- Day11_ExceptionHandling/exception_handling.py
+++ Day11_ExceptionHandling/exception_handling.py
@@ -4,3 +4,13 @@ try:
     print("You Entered: ", num)
 except ValueError:
     print("Invalid Input! Please enter a number") 
+
+# * Multiple exceptions
+try:
+    a = 10
+    b=0
+    print(a/b)
+except ZeroDivisionError:
+    print("Divison by zero is not allowed")        
+except Exception as e:  
+    print("Some other error occured: ",e)  
