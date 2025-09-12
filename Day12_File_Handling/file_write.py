28cf648a2e44b97fbbb04b2d6ae676439849608f
Amit Bisht <am.bisht2003@gmail.com>
2025-09-12 16:52:38 +0530

    Added Day12_File_Handling: file_write.py file

--- Day12_File_Handling/file_write.py
+++ Day12_File_Handling/file_write.py
@@ -4,3 +4,10 @@
 with open("C:\\Users\\ambis\\OneDrive\\Desktop\\Python Journey\\PYTHON_JOURNEY\\Day12_File_Handling\\sample.txt", "w") as f:
     f.write("Hello, this is my first file write!\n")
     f.write("Python makes file handling easy.\n")
+
+
+# Append mode ("a") --> adds new content"
+with open("C:\\Users\\ambis\\OneDrive\\Desktop\\Python Journey\\PYTHON_JOURNEY\\Day12_File_Handling\\sample.txt", "w") as f:
+    f.write("This linne is appended.\n")
+
+print("Data written successfully!")    
