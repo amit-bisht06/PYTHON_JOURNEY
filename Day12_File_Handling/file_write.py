1c646a239e0b390acc5ecc33288bbf9a58f23842
Amit Bisht <am.bisht2003@gmail.com>
2025-09-12 16:51:19 +0530

    Added Day12_File_Handling: file_write.py file

---
+++ Day12_File_Handling/file_write.py
@@ -0,0 +1,6 @@
+# Writing to a file
+
+# Write mode ("w") --> Overwrites file
+with open("C:\\Users\\ambis\\OneDrive\\Desktop\\Python Journey\\PYTHON_JOURNEY\\Day12_File_Handling\\sample.txt", "w") as f:
+    f.write("Hello, this is my first file write!\n")
+    f.write("Python makes file handling easy.\n")
