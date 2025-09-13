37d141ed9d0d1a914c580d4dcb48e3131b44852c
Amit Bisht <am.bisht2003@gmail.com>
2025-09-13 15:13:32 +0530

    Added Day 12: OOP Intro (Class & Object, practice problems)

---
+++ Day13_Oops/practice_problems.py
@@ -0,0 +1,13 @@
+"""
+TODO --> Practice Questions:
+
+1. Create a Car class with attributes (brand, model, year) and a method car_info() to display details.
+
+2. Create a Book class with attributes (title, author, price) and a method to print the book details.
+
+3. Create a BankAccount class with attributes (name, balance) and a method deposit() to add money.
+
+4. Create multiple objects of a Laptop class and display their configurations.
+
+5. Create a Teacher class and initialize attributes using __init__(), then print them.
+"""
